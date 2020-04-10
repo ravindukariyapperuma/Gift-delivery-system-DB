6e9eabc957839f9fb609cd9e387fe40676d35c35
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-11 03:25:24 +0530

    create Employee table

--- Gift delivery DB.sql
+++ Gift delivery DB.sql
@@ -15,3 +15,12 @@ create table Vehicle (
 	vehicle_name char(40),
 	CONSTRAINT chk_vehicle_no CHECK (vehicle_no like '[V][0-9][0-9][0-9]'),
 )
+
+create table Employee (
+	Employee_id char(4) PRIMARY KEY,
+	Employee_name varchar(30) NOT NULL,
+	contact_no char(10) NOT NULL,
+	vehicle_no char(10) FOREIGN KEY REFERENCES Vehicle,
+	CONSTRAINT chk_Employee_id CHECK (Employee_id like '[E][0-9][0-9][0-9]'),
+	CONSTRAINT chk_contact_no_emp CHECK (contact_no not like '%[^0-9]%'),
+)
